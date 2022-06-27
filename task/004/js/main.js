e7dc542858a2d6bea74d7d166c7d3ecb877ab306
jasonKim <kimyk96@gmail.com>
2022-06-27 17:18:34 +0900

    220627

---
+++ task/004/js/main.js
@@ -0,0 +1,230 @@
+$(window).on("load", () => {
+    // url 받아오기
+    href = location.href;
+    if (href.includes("pageno")) {
+        let pageno = href.split("pageno=");
+        let no = pageno[1].split("&");
+        getContact(no[0]);
+    } else {
+        getContact(1);
+    }
+
+    // file 변경 감지 - 업로드 이미지 표시
+    $("#fileUpload").change(function () {
+        let $img = $("#uploadImg")
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $img.attr('src', e.target.result);
+            $img.attr('alt', 'uploadImg');
+        };
+        reader.readAsDataURL(this.files[0]);
+    })
+
+    $("body").on("change", "input.info_imgInput", function () {
+        let no = this.dataset.no;
+        let $img = $(`#img${no}`)
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $img.attr('src', e.target.result);
+            $img.attr('alt', no);
+        };
+        reader.readAsDataURL(this.files[0]);
+    })
+
+    $("#list").on("change", "input", function () {
+        let $li = $(this).closest('li');
+        console.log(this)
+        $li.css("border", "2px solid red");
+    })
+});
+
+// 연락처 받아오기
+function getContact(pageno) {
+    $.ajax({
+        url: "https://contactsvc.herokuapp.com/contacts/",
+        type: "GET",
+        data: { pageno: pageno, pagesize: 5 },
+    })
+        .done((result) => {
+            const resultArray = result.contacts;
+            const $list = $("#list");
+
+            for (const person of resultArray) {
+                const $LI = $(`<li></li>`)
+                    .addClass("item")
+                    .attr("data-no", `${person.no}`)
+                    .appendTo($list);
+                const $DIV = $(`<form id="f${person.no}"></form>`)
+                    .addClass("person")
+                    .appendTo($LI);
+                const $LABEL = $(`<label for="input${person.no}"></label>`).appendTo($DIV);
+                const $IMG = $(`<img>`)
+                    .addClass("person_photo")
+                    .attr("id", `img${person.no}`)
+                    .attr("src", `${person.photo}`)
+                    .attr("alt", `${person.name}`)
+                    .appendTo($LABEL);
+                const $INFO = $(`<div></div>`).addClass("person_info").appendTo($DIV);
+                $(`<input type="file" name="photo" accept="image/png, image/jpeg, image/gif">`)
+                    .attr("id", `input${person.no}`)
+                    .attr("data-no", `${person.no}`)
+                    .addClass("info_imgInput")
+                    .appendTo($INFO);
+                $(`<input disabled>`)
+                    .attr("name", `${person.no}`)
+                    .val(`no : ${person.no}`)
+                    .addClass("info_no")
+                    .appendTo($INFO);
+                $(`<input>`)
+                    .attr("name", `name`)
+                    .val(`${person.name}`)
+                    .addClass("info_name")
+                    .appendTo($INFO);
+                $(`<input>`)
+                    .attr("name", `tel`)
+                    .val(`${person.tel}`)
+                    .addClass("info_tel")
+                    .appendTo($INFO);
+                $(`<input>`)
+                    .attr("name", `address`)
+                    .val(`${person.address}`)
+                    .addClass("info_address")
+                    .appendTo($INFO);
+                const $BTN = $(`<div></div>`).addClass("info_btnarea").appendTo($INFO);
+                $(`<button>수정</button>`)
+                    .addClass("info_edit")
+                    .attr("onclick", `putContact(${person.no}); return false;`)
+                    .appendTo($BTN);
+                $(`<button>삭제</button>`)
+                    .addClass("info_delete")
+                    .attr("onclick", `deleteContact(${person.no}); return false;`)
+                    .appendTo($BTN);
+            }
+
+            // 페이징 처리
+            pagination(result.pageno, result.pagesize, result.totalcount);
+        })
+        .fail({});
+}
+
+// 연락처 - 추가
+function postContact() {
+    let name = $(`#addName`);
+    let tel = $(`#addTel`);
+    let address = $(`#addAdd`);
+    if (!name.val()) {
+        alert("이름은 필수 항목입니다.");
+        name.focus();
+        return;
+    } else if (!tel.val()) {
+        alert("번호는 필수 항목입니다.");
+        tel.focus();
+        return;
+    } else if (!address.val()) {
+        alert("주소는 필수 항목입니다.");
+        address.focus();
+        return;
+    } else {
+        let formData = $(`#contact1`).serialize();
+        $.ajax({
+            url: "https://contactsvc.herokuapp.com/contacts/",
+            type: "POST",
+            data: formData,
+        }).done((result) => {
+            let input = $('#fileUpload')[0];
+            if (input.files[0] == undefined) {
+                location.reload();
+            } else {
+                putContactPhoto(result.no, '#fileUpload');
+            }
+        });
+    }
+}
+
+// 연락처 - 삭제
+function deleteContact(no) {
+    $.ajax({
+        url: "https://contactsvc.herokuapp.com/contacts/" + no,
+        type: "DELETE",
+    })
+        .done((result) => {
+            $(`#f${no}`).parent().empty();
+            alert("연락처 삭제 완료!");
+        })
+        .fail((result) => {
+            console.log(result);
+        });
+}
+
+// 연락처 - 수정
+function putContact(no) {
+    let formData = $(`#f${no}`).serialize();
+    $.ajax({
+        url: "https://contactsvc.herokuapp.com/contacts/" + no,
+        type: "PUT",
+        data: formData,
+    }).done((result) => {
+        let input = $(`#input${no}`)[0];
+        if (input.files[0] == undefined) {
+            location.reload();
+        } else {
+            putContactPhoto(result.no, `#input${no}`)
+        }
+    }).fail((result) => {
+        console.log(result);
+    });
+}
+
+// 연락처 - 사진수정
+function putContactPhoto(no, input) {
+    // 이미지 업로드
+    let fd = new FormData();
+    let img = $(input)[0];
+    fd.append('photo', img.files[0]);
+    fd.append('no', no);
+
+    $.ajax({
+        url: `https://contactsvc.herokuapp.com/contacts/${no}/photo`,
+        type: "POST",
+        data: fd,
+        contentType: false,
+        processData: false,
+    }).done((result) => {
+        location.reload();
+    })
+}
+
+// 페이징 처리
+function pagination(pageno, pagesize, total) {
+    const $page = $("#pagination");
+    const totalPage = Math.ceil(total / pagesize);
+    let pageBlock = 3;
+
+
+    // 가운데 버튼
+    for (let i = pageno-2; i < pageno+pageBlock; i++) {
+        if (i == pageno) {
+            $page.append(`<li><a href="?pageno=${i}" class="now">${i}</a></li>`);
+        } else if (i > 0 && i <= totalPage) {
+            $page.append(`<li><a href="?pageno=${i}">${i}</a></li>`);
+        };
+        if (i < 1) {
+            pageBlock++;
+        };
+        if (i > totalPage) {
+            const current = $("#pagination li:nth-child(1)").text();
+            $page.prepend(`<li><a href="?pageno=${current-1}">${current-1}</a></li>`);
+        }
+    }
+
+
+    // 이전 버튼
+    if (pageno > pageBlock) {
+        $page.prepend(`<li><a href="?pageno=${pageno - pageBlock}">&lt&lt</a></li>`);
+    }
+
+    // 다음 버튼
+    if (pageno <= totalPage - pageBlock) {
+        $page.append(`<li><a href="?pageno=${pageno + pageBlock}">&gt&gt</a></li>`);
+    }
+}
---
+++ task/005/js/main.js
@@ -0,0 +1,176 @@
+// 날씨 파싱
+function getWeather(city) {
+    docOpacity1()
+
+    var url = new URL(location.href);
+    if (city.lat === null) {
+        city = { lon: 126.7052, lat: 37.456, name: 'Incheon', country: 'KR' }
+        url.searchParams.append('lon', city.lon);
+        url.searchParams.append('lat', city.lat);
+        url.searchParams.append('name', city.name);
+        url.searchParams.append('country', city.country);
+    } else {
+        url.searchParams.set('lon', city.lon);
+        url.searchParams.set('lat', city.lat);
+        url.searchParams.set('name', city.name);
+        url.searchParams.set('country', city.country);
+    }
+    history.pushState(city, '', url);
+    $("#cityname").val(`${city.name}, ${city.country}`);
+
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall",
+        type: "GET",
+        data: {
+            lon: city.lon,
+            lat: city.lat,
+            units: "metric",
+            exclude: "minutely,alerts",
+            appid: "b172e2b1e655b4a821c0f13d0574c7ad",
+            lang: "kr",
+        },
+    }).done((res) => {
+        updateToday(res.current);
+        updateHourly(res.hourly);
+        updateDaily(res.daily);
+    }).fail(() => { });
+}
+
+// 날씨 갱신 - 오늘간략
+function updateToday(res) {
+    const today = new Date(res.dt * 1000)
+    const day = new Intl.DateTimeFormat("ko-KR", { weekday: "long" }).format(today);
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = new Intl.DateTimeFormat("en-US", { month: "long" }).format(today);
+    const sunrise = new Date(res.sunrise * 1000);
+    const sunset = new Date(res.sunset * 1000);
+    $("#breif .number").text(parseInt(res.temp));
+    $("#breif .today_desc").text(res.weather[0].description);
+    $("header .today_date").text(`${day}, ${dd} ${mm}`);
+    $(".today_wind").html(`${res.wind_speed.toFixed(1)}km/h`);
+    $(".today_humidity").html(`${res.humidity}%`);
+    $(".today_cloud").html(`${res.clouds}%`);
+    $("#breif .today_maingif").attr("src", `img/${res.weather[0].main}.gif`);
+    $("#sun .sunrise").text(`${sunrise.getHours()}:${sunrise.getMinutes()}`);
+    $("#sun .sunset").text(`${sunset.getHours()}:${sunset.getMinutes()}`);
+}
+
+// 날씨 갱신 - 오늘시간별
+function updateHourly(weather) {
+    let $hourly = $("#hourly ul"); $hourly.empty();
+    let labels = []; let datasets = [];
+
+    // 차트초기화
+    myChart.data.datasets[0].data = [];
+    myChart.data.labels = [];
+
+    for (const [index, value] of weather.entries()) {
+        if (index == 0) {
+            continue;
+        } else if (index > 24) {
+
+        } else if (index % 3 == 0) {
+            let hour = new Date(value.dt * 1000).getHours();
+            if (hour > 12) { hour = (hour - 12) + "PM"; } else { hour = hour + "AM" }
+            labels.push(hour); datasets.push(Math.round(value.temp))
+            $hourly.append(`<li><img src="img/${value.weather[0].main}.gif"></li>`);
+        }
+    }
+    // 차트 갱신
+    myChart.data.datasets[0].data = datasets;
+    myChart.data.labels = labels;
+    myChart.update();
+}
+
+// 날씨 갱신 - 요일별
+function updateDaily(weather) {
+    const $daily = $("#daily ul"); $daily.empty();
+
+    for (const [index, value] of weather.entries()) {
+        if (index == 0) {
+            continue;
+        }
+        let dayNow = new Date(value.dt * 1000);
+        daynow = new Intl.DateTimeFormat("ko-KR", { weekday: "long" }).format(dayNow);
+        let li = $("<li></li>").appendTo($daily);
+        $("<p></p>").html(`${daynow}`).addClass("daily_date strong").appendTo(li);
+        $("<p></p>")
+            .html(`<img src="img/${value.weather[0].main}.gif">`)
+            .addClass("daily_gif")
+            .appendTo(li);
+        $("<p></p>")
+            .html(`${value.weather[0].description}`)
+            .addClass("daily_desc")
+            .appendTo(li);
+        $("<p></p>")
+            .html(`${parseInt(value.temp.max)}°`)
+            .addClass("daily_max")
+            .appendTo(li);
+        $("<p></p>")
+            .html(`${parseInt(value.temp.min)}°`)
+            .addClass("daily_min")
+            .appendTo(li);
+    }
+}
+
+// 도시 검색 - 문자별
+function searchCity(val) {
+    $("main").css("opacity", "0.01"); // main 흐림 효과
+    $.ajax({
+        url: "https://api.openweathermap.org/geo/1.0/direct",
+        type: "GET",
+        data: {
+            q: `${val}`,
+            appid: "b172e2b1e655b4a821c0f13d0574c7ad",
+            limit: 4
+        },
+    }).done((res) => {
+        recommendList(res); // 리스트 노출
+    }).fail(() => { });
+}
+
+// 도시 노출 - 추천 리스트
+function recommendList(res) {
+    const $locationList = $("#locationList"); $locationList.empty();
+    for (const value of res) {
+        const city = { lon: value.lon, lat: value.lat, name: value.name, country: value.country };
+        $(`<li>${value.name}, ${value.country}</li>`).attr("onclick", `getWeather(${JSON.stringify(city)})`).appendTo($locationList);
+    }
+}
+
+// main 밝게
+function docOpacity1() {
+    $("main").css("opacity", "1");
+    $("#locationList").empty();
+};
+
+// main 어둡게
+function docOpacity0() {
+    $("main").css("opacity", "0");
+};
+
+
+// $('svg.radial-progress').each(function (index, value) {
+//     $(this).find($('circle.complete')).removeAttr('style');
+// });
+
+// $(window).scroll(function () {
+//     $('svg.radial-progress').each(function (index, value) {
+//         // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+//         if (
+//             $(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+//             $(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+//         ) {
+//             // Get percentage of progress
+//             percent = $(value).data('percentage');
+//             // Get radius of the svg's circle.complete
+//             radius = $(this).find($('circle.complete')).attr('r');
+//             // Get circumference (2πr)
+//             circumference = 2 * Math.PI * radius;
+//             // Get stroke-dashoffset value based on the percentage of the circumference
+//             strokeDashOffset = circumference - ((percent * circumference) / 100);
+//             // Transition progress for 1.25 seconds
+//             $(this).find($('circle.complete')).animate({ 'stroke-dashoffset': strokeDashOffset }, 1250);
+//         }
+//     });
+// }).trigger('scroll');
